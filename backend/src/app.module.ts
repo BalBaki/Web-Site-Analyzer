4d7d7c020aac22e1982f52f20911f51e69ddf053
Baki <bakibal1994@gmail.com>
2025-02-04 13:28:14 +0300

    Added security middlewares

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-    imports: [],
+    imports: [
+        ThrottlerModule.forRoot([
+            {
+                ttl: 60 * 1000, //60 seconds as miliseconds
+                limit: 2,
+            },
+        ]),
+    ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+        AppService,
+    ],
 })
 export class AppModule {}
